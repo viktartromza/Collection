036007f3da56757b76a56612fb28cd814eee4fbb
ViktarTromza <viktar.tromza@gmail.com>
2022-11-09 07:36:55 +0300

    Modified

--- src/AnimalCollection.java
+++ src/AnimalCollection.java
@@ -2,17 +2,8 @@ import java.awt.*;
 import java.util.LinkedList;
 
 public class AnimalCollection<String> extends LinkedList {
-    String animalName;
 
-    public String getAnimalName() {
-        return animalName;
-    }
-
-    public void setAnimalName(String animalName) {
-        this.animalName = animalName;
-    }
-
-    public void addAnimal(Object o) {
+        public void addAnimal(Object o) {
         super.addLast(o);
     }
 
--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,5 @@ public class Main {
         System.out.println(animalCollection);
         animalCollection.removeAnimal();
         System.out.println(animalCollection);
-            }
+    }
 }
